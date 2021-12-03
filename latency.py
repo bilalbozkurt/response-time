14ce24f5c0c619d9c812f9692543239bbc52046d
bilalbozkurt <mbilalbozkurt@gmail.com>
2021-12-03 20:45:43 +0300

    exceptions handled

--- latency.py
+++ latency.py
@@ -1,31 +1,42 @@
 import sys
 import requests
 from requests.models import ReadTimeoutError
+from requests.packages.urllib3.exceptions import ConnectTimeoutError, MaxRetryError
 
-try:
-    url = sys.argv[1]
-    attempts = int(sys.argv[2])
-    timeout = 50
-    total_time = 0
-    max_time = -1
-    min_time = sys.maxsize
-    for i in range(0, attempts):
+url = sys.argv[1]
+attempts = int(sys.argv[2])
+timeout = 1
+total_time = 0
+max_time = -1
+min_time = sys.maxsize
+for i in range(0, attempts):
+    try:
         response = requests.get(url, timeout=timeout)
         total_time += response.elapsed.total_seconds()
-        if response.elapsed.total_seconds() < min_time:
+        if (response.elapsed.total_seconds() < min_time):
             min_time = response.elapsed.total_seconds()
-        if response.elapsed.total_seconds() > max_time:
+        if (response.elapsed.total_seconds() > max_time):
             max_time = response.elapsed.total_seconds()
-        print('{:10d}{:10f}{:10}'.format(
+        print('{:12d}{:10f}{:10}'.format(
             i+1, response.elapsed.total_seconds(), ' s'))
-    print('{:10}'.format(
-        'Results'))
-    print('{:10}{:10f}{:10}'.format(
-        'Min', min_time, ' s'))
-    print('{:10}{:10f}{:10}'.format(
-        'Max', max_time, ' s'))
-    print('{:10}{:10f}{:10}'.format(
-        'Average', total_time/attempts, ' s'))
-except IndexError:
-    print('Usage: latency.py URL Number_Of_Attempts')
-    exit(0)
+
+    except IndexError:
+        print('Usage: latency.py URL Number_Of_Attempts')
+        exit(0)
+    except requests.exceptions.ReadTimeout:
+        print('{:12d}{:10}'.format(i+1, '  Timeout'))
+        pass
+    except requests.exceptions.ConnectTimeout:
+        print('{:12d}{:10}'.format(i+1, '  Timeout'))
+        pass
+    except MaxRetryError:
+        print('{:12d}{:10}'.format(i+1, '  Timeout'))
+        pass
+print('{:10}'.format(
+    'Results'))
+print('{:10}{:10f}{:10}'.format(
+    'Min', min_time, ' s'))
+print('{:10}{:10f}{:10}'.format(
+    'Max', max_time, ' s'))
+print('{:10}{:10f}{:10}'.format(
+    'Average', total_time/attempts, ' s'))
